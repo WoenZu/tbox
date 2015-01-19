0babee76ebd4079804bc5a695555c96b065dd38c
Woenzu <0x81zu@gmail.com>
2015-01-20 00:33:06 +0300

    reference of config.js removed from index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var utils = require( './utils' );
-var config = require( './config' ).Config;
 var fabric = require( './fabric').MessageFabric;
 var encoder = require( './encoder').Encoder;
 var userinfo = require( './userinfo').UserInfo;
 var client = require( './client').Client;
 
 exports.utils = utils;
-exports.Config = config;
 exports.MessageFabric = fabric;
 exports.Encoder = encoder;
 exports.UserInfo = userinfo;
