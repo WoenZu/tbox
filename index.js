64d03ba2baddbb6a8035215de37dea3b5aa88136
Woenzu <0x81zu@gmail.com>
2015-01-29 00:06:13 +0300

    index.js update for new modules in tbox

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var fabric = require( './fabric').MessageFabric;
 var encoder = require( './encoder').Encoder;
 var userinfo = require( './userinfo').UserInfo;
 var client = require( './client').Client;
+var socketpool = require( './socketpool').SocketPool;
+var userDB = require( './userDB' ).UserDB;
+
+var proto = require( './protocol' ).Protocol;
 
 exports.utils = utils;
 exports.MessageFabric = fabric;
 exports.Encoder = encoder;
 exports.UserInfo = userinfo;
 exports.Client = client;
+exports.Protocol = proto;
+exports.SocketPool = socketpool;
+exports.UserDB = userDB;
 
 //console.log( 'index.js> tbox is loaded...' );
