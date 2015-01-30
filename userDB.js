e255f9c01cf83eb896a656b3dd1dc364a3d2e53f
Woenzu <0x81zu@gmail.com>
2015-01-30 19:57:59 +0300

    update

--- userDB.js
+++ userDB.js
@@ -1,54 +1,47 @@
 'use strict';
 var utils = require( './utils' );
 
-var checkFile = utils.checkFile;
+var loadFile = utils.loadFile;
 
 function UserDB( filePath ) {
-  // if filePath= null then filepath ?????
   var path = filePath;
   var userDB = {};
-
-  var defaultDB = {};
-  defaultDB.users = [
-    {
-      'IP': '127.0.0.1',
-      'nickname': 'admin',
-      'access': 0,
-      'avatar': 'mmmm.jpg',
-      'status': 'active'
-    }];
-
-  userDB = checkFile( path, defaultDB );
 
   this.setDBPath = function( p ) {
     path = p;
   };
 
-  /**
-   *
-   * @param user - user  object, for format see defaultDB
-   */
-  this.addUser = function( user ) {
-    userDB.users.push( usr );
-  };
+  this.createUser = function( userName, userIP, access ) {
+    if( isNaN(access) ) {
+      access = 3;
+    }
 
-  this.createUser = function( userName, userIP ) {
-    //TODO провурка валидности ip адреса и никнейма
+    //TODO проверка валидности ip адреса и никнейма
+
     var newUser = {};
     newUser.IP = userIP;
     newUser.nickname = userName;
-    newUser.access = 3; //regular user - 3, moderator - 2, administrator - 0
+    newUser.access = access;
     newUser.avatar = '';
     newUser.status = 'pending'; // pending, active, banned
     return newUser;
   };
 
+  this.addUser = function( user ) {
+    userDB.users.push( usr );
+  };
 
-  // загрузка базы из файла
+  this.loadDB = function( path, defaultDB) {
+    userDB = loadFile( path, defaultDB );//TODO refactor (loadFile func)
+  };
 
-  // запись объекта базы в файл, асинхронно
+  this.saveDB = function( db ) {
+    // запись объекта базы в файл, асинхронно
+  };
 
+  this.checkDBForUser = function( nick, ip ) {
 
+  };
 }
 
 exports.UserDB = UserDB;
