0980e00405b8922e1252d8eddd7c5519e3078218
Woenzu <0x81zu@gmail.com>
2015-02-10 21:01:40 +0300

    getClientById() function

--- socketpool.js
+++ socketpool.js
@@ -6,6 +6,22 @@ function SocketPool() {
   this.push = function( sock ) {
     pool.push( sock );
   };
+
+  this.getClientById = function( id ) {
+    var ip = id.ip.split( /\:/ ); //127.0.0.1:6666
+
+    for ( var i = 0; i < pool.length; i++ ) {
+      if ( pool[ i ].getIP() == ip[ 0 ] ) {
+        if ( pool[ i ].getPort() == ip[ 1 ] ) { return pool[i]; }
+      } else {
+        return null;
+      }
+    }
+  };
+
+  this.getPool = function() {
+    return pool;
+  };
 }
 
 exports.SocketPool = SocketPool;
