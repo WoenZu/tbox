0684146ee2fea0f587ab0e065a6f51ee8db3520a
Woenzu <0x81zu@gmail.com>
2015-02-26 00:49:41 +0300

    code style changed

--- clientpool.js
+++ clientpool.js
@@ -1,46 +1,47 @@
 'use strict';
 
-var tutils = require( './tutils' );
+var tutils = require('./tutils');
 var splitIdent = tutils.splitIdent;
 
 function ClientPool() {
   var pool = [];
 
-  this.addClient = function( client ) {
-    pool.push( client );
+  this.addClient = function(client) {
+    pool.push(client);
   };
 
-  this.removeClient = function( client ) {
-    var  index = this.getClientIndex( client );
+  this.removeClient = function(client) {
+    var  index = this.getClientIndex(client);
     try {
-      pool.splice( index, 1);
-    } catch ( err ) {
-      console.log( err );
-      console.log( '[ERROR] Client is not found.' );
+      pool.splice(index, 1);
+    } catch (err) {
+      console.log(err);
+      console.log('[ERROR] Client is not found.');
     }
   };
 
-  this.getClientById = function( ident ) {
-    var id = splitIdent( ident );
-    var ip = id[ 0 ];
-    var port = id[ 1 ];
+  this.getClientById = function(ident) {
+    var id = splitIdent(ident);
+    var ip = id[0];
+    var port = id[1];
 
-    for ( var i = 0; i < pool.length; i++ ) {
-      if ( pool[ i ].getIP() == ip ) {
-        if ( pool[ i ].getPort() == port ) { return pool[ i ]; }
+    for (var i = 0; i < pool.length; i++) {
+      if (pool[i].getIP() === ip) {
+        if (pool[i].getPort() === port) {
+          return pool[i]; }
       } else {
         return null;
       }
     }
   };
 
-  this.getClientIndex = function( client ) {
+  this.getClientIndex = function(client) {
     var ip = client.getIP();
     var port = client.getPort();
 
-    for ( var i = 0; i < pool.length; i++ ) {
-      if ( pool[ i ].getIP() == ip ) {
-        if ( pool[ i ].getPort() == port ) { return i; }
+    for (var i = 0; i < pool.length; i++) {
+      if (pool[i].getIP() === ip) {
+        if (pool[i].getPort() === port) { return i; }
       } else {
         return null;
       }
