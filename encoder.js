81714ad3bfffe9c715f0014700f8514325f28794
Woenzu <0x81zu@gmail.com>
2015-03-03 23:43:05 +0300

    refactoring

--- encoder.js
+++ encoder.js
@@ -11,52 +11,52 @@
 
 'use strict';
 
-function Encoder( key ) {
-  this.key = key;
+function Encoder(k) {
+  var key = k;
+
+  this.encode = function (str) {
+
+    var s = [];
+    var j = 0;
+    var x;
+    var res = '';
+
+    for (var i = 0; i < 256; i++) {
+      s[i] = i;
+    }
+
+    for (i = 0; i < 256; i++) {
+      j = ( j + s[i] + key.charCodeAt(i % key.length) ) % 256;
+
+      x = s[i];
+      s[i] = s[j];
+      s[j] = x;
+    }
+
+    i = 0;
+    j = 0;
+
+    for (var y = 0; y < str.length; y++) {
+      i = ( i + 1 ) % 256;
+      j = ( j + s[i]) % 256;
+
+      x = s[i];
+      s[i] = s[j];
+      s[j] = x;
+
+      res += String.fromCharCode(str.charCodeAt(y) ^ s[( s[i] + s[j] ) % 256]);
+    }
+
+    return res;
+  };
+
+  this.setKey = function (k) {
+    key = k;
+  };
+
+  this.getKey = function () {
+    return key;
+  };
 }
-
-Encoder.prototype.encode = function( str ) {
-
-  var s = [];
-  var j = 0;
-  var x;
-  var res = '';
-
-  for ( var i = 0; i < 256; i++ ) {
-    s[ i ] = i;
-  }
-
-  for ( i = 0; i < 256; i++ ) {
-    j = ( j + s[ i ] + this.key.charCodeAt( i % this.key.length ) ) % 256;
-
-    x = s[ i ];
-    s[ i ] = s[ j ];
-    s[ j ] = x;
-  }
-
-  i = 0;
-  j = 0;
-
-  for ( var y = 0; y < str.length; y++ ) {
-    i = ( i + 1 ) % 256;
-    j = ( j + s[ i ]) % 256;
-
-    x = s[ i ];
-    s[ i ] = s[ j ];
-    s[ j ] = x;
-
-    res += String.fromCharCode( str.charCodeAt( y ) ^ s[ ( s[ i ] + s[ j ] ) % 256 ] );
-  }
-
-  return res;
-};
-
-Encoder.prototype.setKey = function( key ) {
-  this.key = key;
-};
-
-Encoder.prototype.getKey = function() {
-  return this.key;
-};
 
 exports.Encoder = Encoder;
--- userinfo.js
+++ userinfo.js
@@ -3,43 +3,43 @@
 var os = require('os');
 
 function UserInfo() {
-  this.IP = '';
-  this.userName = '';
-  this.host = '';
+  var IP = '';
+  var userName = '';
+  var host = '';
 
-  if( process.platform === 'win32' ) {
+  if (process.platform === 'win32') {
 
-    this.IP = os.networkInterfaces().Ethernet[1].address;
-    this.userName = process.env.USERNAME;
-    this.host = os.hostname();
+    IP = os.networkInterfaces().Ethernet[1].address;
+    userName = process.env.USERNAME;
+    host = os.hostname();
 
   } else if (process.platform === 'darwin') {
-    this.IP = os.networkInterfaces().en0[1].address;
-    this.userName = process.env.USER;
-    this.host = os.hostname();
+    IP = os.networkInterfaces().en0[1].address;
+    userName = process.env.USER;
+    host = os.hostname();
 
   } else {
     // linux
-    // this.IP =
-    // this.userName =
-    // this.host =
+    // P =
+    // userName =
+    // host =
   }
+
+  this.getUserName = function () {
+    return userName;
+  };
+
+  this.getIP = function () {
+    return IP;
+  };
+
+  this.getHostName = function () {
+    return host;
+  };
+
+  this.getUserInfo = function () {
+    return IP + '/' + host + '/' + userName;
+  };
 }
-
-UserInfo.prototype.getUserName = function() {
-  return this.userName;
-};
-
-UserInfo.prototype.getIP = function() {
-  return this.IP;
-};
-
-UserInfo.prototype.getHostName = function(){
-  return this.host;
-};
-
-UserInfo.prototype.getUserInfo = function() {
-  return this.IP + '/' + this.host + '/' + this.userName;
-};
 
 exports.UserInfo = UserInfo;
