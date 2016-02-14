15c25968e12e28500340b7893e8fd15b86d38620
Woenzu <0x81zu@gmail.com>
2016-02-14 20:47:38 +0300

    added config.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var client = require('./client').Client;
 var clientpool = require('./clientpool').ClientPool;
 var userDB = require('./userDB').UserDB;
 var proto = require('./protocol').Protocol;
+var config = require('./config').Config;
 
 exports.tutils = tutils;
 exports.Encoder = encoder;
 exports.UserInfo = userinfo;
 exports.Client = client;
-exports.Protocol = proto;
 exports.ClientPool = clientpool;
 exports.UserDB = userDB;
+exports.Protocol = proto;
+exports.Config = config;
